5ca6d6cc199906d72376064cd28d89332fe7bc62
ameetsaahu <ameetsaahu@outlook.com>
2021-08-04 09:01:28 +0530

    Added circlecityconCTF2021

---
+++ circlecityconCTF2021/sockcamp/x86_64-buildroot-linux-uclibc_sdk-buildroot/lib/gcc/x86_64-buildroot-linux-uclibc/9.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/plushie/Downloads/buildroot-2021.02.1/output/host --sysconfdir=/home/plushie/Downloads/buildroot-2021.02.1/output/host/etc --enable-static --target=x86_64-buildroot-linux-uclibc --with-sysroot=/home/plushie/Downloads/buildroot-2021.02.1/output/host/x86_64-buildroot-linux-uclibc/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/plushie/Downloads/buildroot-2021.02.1/output/host --with-mpc=/home/plushie/Downloads/buildroot-2021.02.1/output/host --with-mpfr=/home/plushie/Downloads/buildroot-2021.02.1/output/host --with-pkgversion='Buildroot 2021.02.1' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --enable-tls --enable-threads --without-isl --without-cloog --with-arch=nocona --enable-languages=c --with-build-time-tools=/home/plushie/Downloads/buildroot-2021.02.1/output/host/x86_64-buildroot-linux-uclibc/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "nocona" } };
